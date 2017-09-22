40a0d939e6b0c75115b09b9a9df2d14bbf1052e9
Chris Dyer <chrisdyer1@gmail.com>
2017-09-22 20:43:48 +0100

    Initial Commit

---
+++ Doughnut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Doughnut
+//
+//  Created by Chris Dyer on 22/09/2017.
+//  Copyright © 2017 Chris Dyer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Doughnut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Doughnut
+//
+//  Created by Chris Dyer on 22/09/2017.
+//  Copyright © 2017 Chris Dyer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
