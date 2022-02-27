80a6b9f5e7b1f43204f59cff76ea3ff545e0f7d0
Chris Dyer <chrisdyer1@gmail.com>
2022-02-27 20:24:32 +0000

    Update copyrights, add credits to about window and cut beta release.

--- Doughnut/Windows/ShowEpisodeWindow.swift
+++ Doughnut/Windows/ShowEpisodeWindow.swift
@@ -1,6 +1,6 @@
 /*
  * Doughnut Podcast Client
- * Copyright (C) 2017 Chris Dyer
+ * Copyright (C) 2017 - 2022 Chris Dyer
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
