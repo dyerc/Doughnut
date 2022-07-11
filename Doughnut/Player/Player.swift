4b0fe8f479a81c59d0412b7bf4974b330bb09cf5
Ethan Wong <e1hanw0ng@gmail.com>
2022-07-12 01:33:09 +0800

    Implement custom User-Agent for media playback.

--- Doughnut/Player/Player.swift
+++ Doughnut/Player/Player.swift
@@ -77,6 +77,8 @@ final class Player: NSObject {
 
   private var periodicTimeObservers = [Any]()
 
+  private let userAgent: String = buildUserAgent()
+
   override init() {
     super.init()
     setupRemoteCommands()
@@ -105,7 +107,12 @@ final class Player: NSObject {
       guard let url = URL(string: enclosureUrl) else { return }
 
       currentPlaybackURL = url
-      avAsset = AVAsset(url: url)
+      avAsset = AVURLAsset(
+        url: url,
+        options: [
+          "AVURLAssetHTTPHeaderFieldsKey": buildAVPlayerHTTPHeaders(),
+        ]
+      )
     }
 
     currentAVAsset = avAsset
@@ -268,6 +275,25 @@ final class Player: NSObject {
   private func postPlaybackStatusUpdates() {
     delegate?.updatePlayback()
     updateNowPlayingPlaybackInfo()
+  }
+
+  private func buildAVPlayerHTTPHeaders() -> [String: String] {
+    return [
+      "User-Agent": self.userAgent,
+    ]
+  }
+
+  private static func buildUserAgent() -> String {
+    let processInfo = ProcessInfo()
+
+    var bundleVersion = ""
+    var bundleShortVersion = ""
+    if let infoDict = Bundle.main.infoDictionary {
+      bundleVersion = (infoDict["CFBundleVersion"] as? String) ?? ""
+      bundleShortVersion = (infoDict["CFBundleShortVersionString"] as? String) ?? ""
+    }
+
+    return "Doughnut/\(bundleShortVersion).\(bundleVersion) (+https://doughnutapp.com/; Podcast Client; macOS \(processInfo.operatingSystemVersionString))"
   }
 
   // MARK: - Actions
